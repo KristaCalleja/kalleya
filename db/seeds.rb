a2612018609ac4e0dac777d1b053edba0be7623a
KristaCalleja <kristacm@gmail.com>
2020-11-30 16:48:44 -0300

    Added random quotation

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database..."
+Quotation.destroy_all
+puts "Creating quotations, in 3, 2, 1..."
+text_array = [
+    "The Finite yearns for the Infinite. The Infinite creates the Finite to join it to the Infinite, and this forever in a continuous uninterrupted evolution.",
+    "On artistic creation: 'a divine struggle'.",
+    "On the Clown weeping blood: 'this blood will finally cleanse humanity'.",
+    "On the explaining an artwork: 'For me the meaning counts, it counts a lot!'",
+    "Libero e indipendente corro nei cieli inesplorati."
+]
+
+create_quotation = Quotation.create!(
+    text: text_array.sample
+)
+puts "Seeding finished. Watch your plant grow during this neverending quarantine."
