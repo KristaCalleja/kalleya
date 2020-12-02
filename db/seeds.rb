cb1f1442b3ca3b8c84b4d478ee899307fc19f9f3
KristaCalleja <kristacm@gmail.com>
2020-12-01 22:50:18 -0300

    Edited seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,12 +11,18 @@ puts "Creating quotations, in 3, 2, 1..."
 text_array = [
     "The Finite yearns for the Infinite. The Infinite creates the Finite to join it to the Infinite, and this forever in a continuous uninterrupted evolution.",
     "On artistic creation: 'a divine struggle'.",
-    "On the Clown weeping blood: 'this blood will finally cleanse humanity'.",
+    "On the depicted Clown weeping blood: 'this blood will finally cleanse humanity'.",
     "On the explaining an artwork: 'For me the meaning counts, it counts a lot!'",
-    "Libero e indipendente corro nei cieli inesplorati."
+    "Libero e indipendente corro nei cieli inesplorati.",
+    "On tardiness: 'Don't fret about being late. What is one hour compared to waiting for eternity?'",
+    "Hajja wahda m'hijiex bizzejjed ghal dak kollu li jista' johlom il-bniedem.",
+    "Kif tista' tifhmu lil Alla? Tista' ssammar musmar b'rasek? Tkisser rasek u ma ssammarx il-musmar!",
+    "Kif jiena? Jiena hadida tal-lastiku!",
+    "Jekk irrid immur sal-Mellieha, naqbad ta' Birzebbuga.",
+    "Everything must be viewed cosmically; and to that extent I believe in the absoluteness of matter which somehow always exists in God."
 ]
 
-6.times do |i|
+11.times do |i|
     quotation = Quotation.create!(
         text: text_array[i]
     )
