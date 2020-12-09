cb37652acb27bee122e3012b66f00f8513249a5f
KristaCalleja <kristacm@gmail.com>
2020-12-08 22:32:42 -0300

    Upgrade pg version from 12 to 13

--- config/initializers/social_share_button.rb
+++
@@ -1,3 +0,0 @@
-SocialShareButton.configure do |config|
-  config.allow_sites = %w(twitter facebook pinterest email linkedin telegram whatsapp_app whatsapp_web)
-end
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_11_30_135241) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "quotations", force: :cascade do |t|
     t.string "text"
     t.datetime "created_at", precision: 6, null: false
